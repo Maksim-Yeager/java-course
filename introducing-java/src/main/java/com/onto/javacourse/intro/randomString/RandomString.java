9486cf92a197a99d68377b10009125ba100e7409
Maksim-Yeager <maksim12345daniel@gmail.com>
2024-02-25 21:29:27 +0200

    Update RandomString.java

--- introducing-java/src/main/java/com/onto/javacourse/intro/randomString/RandomString.java
+++ introducing-java/src/main/java/com/onto/javacourse/intro/randomString/RandomString.java
@@ -9,13 +9,13 @@ public class RandomString {
      * @param length The length of the random string to generate.
      * @return A random string of the specified length.
      */
-    public static String randomString(int lenght) {
-        if (lenght <= 0) {
+    public static String randomString(int length) {
+        if (length <= 0) {
             throw new IllegalArgumentException("Lenght can not be or less than 0");
         }
         StringBuilder sb = new StringBuilder();
         String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        for (int i = 0; i < lenght; i++) {
+        for (int i = 0; i < length; i++) {
             sb.append(characters.charAt((int) (Math.random() * characters.length())));
         }
         return sb.toString();
