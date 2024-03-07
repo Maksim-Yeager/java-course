01dc119c5873726adc8bacda0d9fae6aa3209351
Maksim-Yeager <maksim12345daniel@gmail.com>
2024-03-07 19:53:08 +0200

    The arrayList is turned into string and the tests are writen better

--- introducing-java/src/main/java/com/onto/javacourse/intro/summingLargeNumbers/SummingLargeNumbers.java
+++ introducing-java/src/main/java/com/onto/javacourse/intro/summingLargeNumbers/SummingLargeNumbers.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * This class provides a method to sum two large numbers represented as strings.
  */
@@ -13,32 +10,75 @@ public class SummingLargeNumbers {
      * @param sNum the second large number
      * @return a list representing the sum of the two large numbers
      */
-    public static List<Integer> sumNumbers(String fNum, String sNum) {
+    public static String sumNumbers(String fNum, String sNum) {
+        if (fNum == null || sNum == null || fNum.isEmpty() || sNum.isEmpty()) {
+            throw new IllegalArgumentException("The array can not be empty or null!!!");
+        }
+        if (!isNumeric(fNum) || !isNumeric(sNum)) {
+            throw new IllegalArgumentException("Input strings must contain only numeric characters");
+        }
+
         if (fNum.length() < sNum.length()) {
             String temp = sNum;
             sNum = fNum;
             fNum = temp;
         }
-
-        List<Integer> sum = new ArrayList<>();
+        String sum = "";
         int carry = 0;
         int sumN = 0;
         for (int i = 0; i < sNum.length(); i++) {
             int fNumber = (int) (fNum.charAt(fNum.length() - 1 - i)) - 48;
             int sNumber = (int) (sNum.charAt(sNum.length() - 1 - i)) - 48;
             sumN = fNumber + sNumber + carry;
-            sum.add(sumN % 10);
+            sum += (sumN % 10);
             carry = sumN / 10;
         }
         for (int i = sNum.length(); i < fNum.length(); i++) {
             int fNumber = (int) (fNum.charAt(fNum.length() - 1 - i)) - 48;
             sumN = fNumber + carry;
-            sum.add(sumN % 10);
+            sum += (sumN % 10);
             carry = sumN / 10;
         }
         if (carry > 0) {
-            sum.add(carry);
+            sum += (carry);
         }
-        return sum.reversed();
+        return stringReverse(sum);
     }
+
+    /**
+     * Reverses the characters in a string.
+     *
+     * @param str The string to be reversed.
+     * @return The reversed string.
+     * @throws IllegalArgumentException If the input string is null or empty.
+     */
+    public static String stringReverse(String srt) {
+        if (srt == null || srt.length() == 0) {
+            throw new IllegalArgumentException("The array can not be empty or null!!!");
+        }
+        int start = 0;
+        char[] chars = srt.toCharArray();
+        while (start <= srt.length() - 1 - start) {
+            char temp = chars[start];
+            chars[start] = chars[chars.length - 1 - start];
+            chars[chars.length - 1 - start] = temp;
+            start++;
+        }
+        srt = "";
+        for (int i = 0; i < chars.length; i++) {
+            srt += chars[i];
+        }
+        return srt;
+    }
+
+    /**
+     * Checks if a string contains only numeric characters.
+     *
+     * @param str The string to be checked.
+     * @return {@code true} if the string contains only numeric characters, {@code false} otherwise.
+     */
+    private static boolean isNumeric(String str) {
+        return str.matches("\\d+");
+    }
+
 }
--- introducing-java/src/test/java/com/onto/javacourse/intro/summingLargeNumbers/SummingLargeNumbersTest.java
+++ introducing-java/src/test/java/com/onto/javacourse/intro/summingLargeNumbers/SummingLargeNumbersTest.java
@@ -1,7 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SummingLargeNumbersTest {
@@ -10,7 +8,43 @@ public class SummingLargeNumbersTest {
     public void sumNumberTest() {
         String fNum = "3734547554351634";
         String sNum = "90327251413";
-        List<Integer> expected = List.of(3, 7, 3, 4, 6, 3, 7, 8, 8, 1, 6, 0, 3, 0, 4, 7);
+        String expected = "3734637881603047";
         assertEquals(expected, SummingLargeNumbers.sumNumbers(fNum, sNum));
+        Exception isNotNumericException = assertThrows(IllegalArgumentException.class, () -> {
+            SummingLargeNumbers.sumNumbers("afasf", "234");
+        });
+        Exception nullException = assertThrows(IllegalArgumentException.class, () -> {
+            SummingLargeNumbers.sumNumbers(null,null);
+        });
+        Exception emptyException = assertThrows(IllegalArgumentException.class, () -> {
+            SummingLargeNumbers.sumNumbers("", "");
+        });
+        String expectedMessage = "The array can not be empty or null!!!";
+        String expectedMessageIsNotNumeric = "Input strings must contain only numeric characters";
+        String actualMessageEmpty = emptyException.getMessage();
+        assertTrue(actualMessageEmpty.contains(expectedMessage));
+        String actualMessageNull = nullException.getMessage();
+        assertTrue(actualMessageNull.contains(expectedMessage));
+        String actualMessageIsNotNumeric = isNotNumericException.getMessage();
+        assertTrue(actualMessageIsNotNumeric.contains(expectedMessageIsNotNumeric));
+    }
+
+    @Test
+    public void stringReverseTest(){
+        String str = "1234567";
+        String expectedStr = "7654321";
+        String reversedStr = SummingLargeNumbers.stringReverse(str);
+        assertEquals(expectedStr, reversedStr);
+        Exception emptyArrayException = assertThrows(IllegalArgumentException.class, () -> {
+            SummingLargeNumbers.stringReverse("");
+        });
+        Exception nullArrayException = assertThrows(IllegalArgumentException.class, () -> {
+            SummingLargeNumbers.stringReverse(null);
+        });
+        String expectedMessage = "The array can not be empty or null!!!";
+        String actualMessageEmpty = emptyArrayException.getMessage();
+        assertTrue(actualMessageEmpty.contains(expectedMessage));
+        String actualMessageNull = nullArrayException.getMessage();
+        assertTrue(actualMessageNull.contains(expectedMessage));
     }
 }
