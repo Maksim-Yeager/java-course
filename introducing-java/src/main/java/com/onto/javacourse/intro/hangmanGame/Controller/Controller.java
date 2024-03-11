afd053ff8ba81a5c338025c49de5c14ab87a7846
Maksim-Yeager <maksim12345daniel@gmail.com>
2024-03-11 21:51:45 +0200

    javaDoc successfully added

--- introducing-java/src/main/java/com/onto/javacourse/intro/hangmanGame/Controller/Controller.java
+++ introducing-java/src/main/java/com/onto/javacourse/intro/hangmanGame/Controller/Controller.java
@@ -3,26 +3,39 @@ package Controller;
 import Model.Model;
 import View.ConsoleView;
 
+/**
+ * The Controller class controls the flow of the Hangman game by coordinating
+ * interactions between the model and the view.
+ */
 public class Controller {
     private Model model;
     private ConsoleView view;
 
+    /**
+     * Constructs a new Controller with the specified model and view.
+     *
+     * @param model The model component of the Hangman game.
+     * @param view  The view component of the Hangman game.
+     */
     public Controller(Model model, ConsoleView view) {
         this.model = model;
         this.view = view;
     }
 
+    /**
+     * Starts a new game of Hangman.
+     */
     public void startGame() {
         model.newGame();
         while (!model.isGameOver()) {
             view.displayWordState(model.getCurrentWordState());
             view.displayAttemptsLeft(model.getAttemptsLeft());
-            char guess = view.getGuess();
+            char guess = view.guess();
             boolean correctGuess = model.makeGuess(guess);
             if (correctGuess) {
-                System.out.println("Correct guess!");
+                System.out.println("Correct guess");
             } else {
-                System.out.println("Incorrect guess!");
+                System.out.println("Incorrect guess");
             }
         }
         view.displayGameOver(model.getCurrentWordState().contains("_"), model.getSecretWord());
--- introducing-java/src/main/java/com/onto/javacourse/intro/hangmanGame/interfaces/ViewInterface.java
+++ introducing-java/src/main/java/com/onto/javacourse/intro/hangmanGame/interfaces/ViewInterface.java
@@ -1,11 +1,39 @@
 package interfaces;
 
-import java.util.Scanner;
-
+/**
+ * The ViewInterface interface represents the view component of the Hangman game.
+ * It defines methods for interacting with the user and displaying game-related information.
+ */
 public interface ViewInterface {
-    Scanner scanner = new Scanner(System.in);
-    char getGuess();
+
+    /**
+     * Gets the user's guess input.
+     *
+     * @return The character representing the user's guess.
+     */
+    char guess();
+
+    /**
+     * Displays the current state of the word being guessed.
+     *
+     * @param wordState The current state of the word, with guessed letters revealed and unguessed letters masked.
+     */
     void displayWordState(String wordState);
+
+    /**
+     * Displays the number of attempts left for the player.
+     *
+     * @param attemptsLeft The number of attempts left for the player to guess the word.
+     */
     void displayAttemptsLeft(int attemptsLeft);
+
+    /**
+     * Displays the game over message indicating whether the player won or lost,
+     * and reveals the secret word if the player lost.
+     *
+     * @param win        Indicates whether the player won the game (true) or lost (false).
+     * @param secretWord The secret word that was being guessed.
+     */
     void displayGameOver(boolean win, String secretWord);
 }
+
